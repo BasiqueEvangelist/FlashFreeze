be48d5d7db84619dc65e70c294e7e37d5b75753a
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-06-26 00:41:28 +0300

    Fix dumb bug

--- src/main/java/me/basiqueevangelist/palettebypass/UnknownBlockState.java
+++ src/main/java/me/basiqueevangelist/palettebypass/UnknownBlockState.java
@@ -4,14 +4,16 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.util.Identifier;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class UnknownBlockState {
-    private Identifier blockId;
-    private Map<String, String> properties;
+    private final Identifier blockId;
+    private final Map<String, String> properties;
 
     public UnknownBlockState(NbtCompound tag) {
         blockId = new Identifier(tag.getString("Name"));
+        properties = new HashMap<>();
         if (tag.contains("Properties", NbtElement.COMPOUND_TYPE)) {
             NbtCompound propertiesTag = tag.getCompound("Properties");
 
