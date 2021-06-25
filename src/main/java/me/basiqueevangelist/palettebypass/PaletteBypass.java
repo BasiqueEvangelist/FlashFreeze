0418a18194d051afadcff348a7ba1991201bd51a
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-06-26 01:41:09 +0300

    Items!

--- src/main/java/me/basiqueevangelist/palettebypass/PaletteBypass.java
+++ src/main/java/me/basiqueevangelist/palettebypass/PaletteBypass.java
@@ -3,6 +3,10 @@ package me.basiqueevangelist.palettebypass;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.LiteralText;
 import org.apache.logging.log4j.LogManager;
 
 public class PaletteBypass implements ModInitializer {
@@ -14,5 +18,12 @@ public class PaletteBypass implements ModInitializer {
     public static BlockState getForUnknown(UnknownBlockState state) {
         // Returns bedrock for now.
         return Blocks.BEDROCK.getDefaultState();
+    }
+
+    public static ItemStack makeFakeStack(NbtCompound tag) {
+        ItemStack stack = new ItemStack(Items.NETHER_STAR);
+        stack.getOrCreateTag().put("OriginalData", tag);
+        stack.setCustomName(new LiteralText("Unknown item: " + tag.getString("id")));
+        return stack;
     }
 }
---
+++ src/main/java/me/basiqueevangelist/palettebypass/mixin/ItemStackMixin.java
@@ -0,0 +1,37 @@
+package me.basiqueevangelist.palettebypass.mixin;
+
+import me.basiqueevangelist.palettebypass.PaletteBypass;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+    @Shadow private NbtCompound tag;
+
+    @Shadow @Nullable public abstract NbtCompound getSubTag(String key);
+
+    @Inject(method = "fromNbt", at = @At("HEAD"), cancellable = true)
+    private static void makeUnknownIfNeeded(NbtCompound tag, CallbackInfoReturnable<ItemStack> cir) {
+        if (tag.contains("id", NbtElement.STRING_TYPE)
+        && !Registry.ITEM.containsId(new Identifier(tag.getString("id")))) {
+            cir.setReturnValue(PaletteBypass.makeFakeStack(tag));
+        }
+    }
+
+    @Inject(method = "writeNbt", at = @At("HEAD"), cancellable = true)
+    private void writeUnknownNbt(NbtCompound tag, CallbackInfoReturnable<NbtCompound> cir) {
+        if (this.tag != null && this.tag.contains("OriginalData", NbtElement.COMPOUND_TYPE)) {
+            tag.copyFrom(getSubTag("OriginalData"));
+            cir.setReturnValue(tag);
+        }
+    }
+}
