152833c4355ae6915b8e2bf5de34116420acdfc3
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-06-28 23:28:54 +0300

    Entities

---
+++ src/main/java/me/basiqueevangelist/palettebypass/FakeArmorStandEntity.java
@@ -0,0 +1,51 @@
+package me.basiqueevangelist.palettebypass;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.text.LiteralText;
+import net.minecraft.world.World;
+
+@SuppressWarnings("EntityConstructor")
+public class FakeArmorStandEntity extends ArmorStandEntity {
+    private NbtCompound originalData;
+
+    public FakeArmorStandEntity(World world, NbtCompound originalData) {
+        super(EntityType.ARMOR_STAND, world);
+        this.originalData = originalData;
+
+        setCustomName(new LiteralText("Unknown entity " + originalData.getString("id")));
+        setCustomNameVisible(true);
+
+        NbtList pos = originalData.getList("Pos", 6);
+        this.setPos(pos.getDouble(0), pos.getDouble(1), pos.getDouble(2));
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+
+        nbt.put("OriginalData", originalData);
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        originalData = nbt.getCompound("OriginalData");
+    }
+
+    @Override
+    public boolean hasNoGravity() {
+        return true;
+    }
+
+    @Override
+    public boolean saveNbt(NbtCompound nbt) {
+        nbt.copyFrom(originalData);
+        return true;
+    }
+
+    public NbtCompound getOriginalData() {
+        return originalData;
+    }
+}
---
+++ src/main/java/me/basiqueevangelist/palettebypass/mixin/EntityTypeMixin.java
@@ -0,0 +1,26 @@
+package me.basiqueevangelist.palettebypass.mixin;
+
+import me.basiqueevangelist.palettebypass.FakeArmorStandEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+@Mixin(EntityType.class)
+public class EntityTypeMixin {
+    @Inject(method = "getEntityFromNbt", at = @At("HEAD"), cancellable = true)
+    private static void makeFakeEntityIfNeeded(NbtCompound nbt, World world, CallbackInfoReturnable<Optional<Entity>> cir) {
+        if (nbt.contains("id", NbtElement.STRING_TYPE) && !Registry.ENTITY_TYPE.containsId(new Identifier(nbt.getString("id")))) {
+            cir.setReturnValue(Optional.of(new FakeArmorStandEntity(world, nbt)));
+        }
+    }
+}
