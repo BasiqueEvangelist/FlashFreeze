90828b3f06c9585e7c9281119f4ae35d68370b92
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-07-14 19:28:01 +0300

    make fake entities invincible to non-creative players and drop a special item that can be used to create another one

--- src/main/java/me/basiqueevangelist/flashfreeze/FakeArmorStandEntity.java
+++ src/main/java/me/basiqueevangelist/flashfreeze/FakeArmorStandEntity.java
@@ -1,9 +1,18 @@
 package me.basiqueevangelist.flashfreeze;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
 import net.minecraft.world.World;
 
@@ -18,8 +27,11 @@ public class FakeArmorStandEntity extends ArmorStandEntity {
         setCustomName(new LiteralText("Unknown entity " + originalData.getString("id")));
         setCustomNameVisible(true);
 
-        NbtList pos = originalData.getList("Pos", 6);
+        NbtList pos = originalData.getList("Pos", NbtElement.DOUBLE_TYPE);
         this.setPos(pos.getDouble(0), pos.getDouble(1), pos.getDouble(2));
+
+        NbtList rot = originalData.getList("Rotation", NbtElement.FLOAT_TYPE);
+        this.setRotation(rot.getFloat(0), rot.getFloat(1));
     }
 
     @Override
@@ -40,8 +52,37 @@ public class FakeArmorStandEntity extends ArmorStandEntity {
     }
 
     @Override
+    public boolean damage(DamageSource source, float amount) {
+        if (this.world.isClient || this.isRemoved()) return false;
+        if (DamageSource.OUT_OF_WORLD.equals(source)) {
+            this.kill();
+            return false;
+        }
+        if (!source.isSourceCreativePlayer()) return false;
+
+        if (source.getAttacker().isSneaking()) {
+            ItemStack droppedStack = new ItemStack(Items.ARMOR_STAND);
+            var newEntityTag = originalData.copy();
+            newEntityTag.remove("UUID");
+            droppedStack.getOrCreateTag().put("OriginalEntityData", newEntityTag);
+            droppedStack.setCustomName(new LiteralText("Unknown entity " + originalData.getString("id")));
+            Block.dropStack(this.world, this.getBlockPos(), droppedStack);
+        }
+        this.onBreak(source);
+        ((ServerWorld)this.world).spawnParticles(new BlockStateParticleEffect(ParticleTypes.BLOCK, Blocks.OAK_PLANKS.getDefaultState()), this.getX(), this.getBodyY(0.6666666666666666), this.getZ(), 10, (double)(this.getWidth() / 4.0F), (double)(this.getHeight() / 4.0F), (double)(this.getWidth() / 4.0F), 0.05);
+        this.kill();
+
+        return true;
+    }
+
+    @Override
     public boolean saveNbt(NbtCompound nbt) {
         nbt.copyFrom(originalData);
+        if (getVehicle() != null)
+            nbt.put("Pos", toNbtList(getVehicle().getX(), getY(), getVehicle().getZ()));
+        else
+            nbt.put("Pos", toNbtList(getX(), getY(), getZ()));
+        nbt.put("Rotation", toNbtList(getYaw(), getPitch()));
         return true;
     }
 
---
+++ src/main/java/me/basiqueevangelist/flashfreeze/mixin/ArmorStandItemMixin.java
@@ -0,0 +1,32 @@
+package me.basiqueevangelist.flashfreeze.mixin;
+
+import me.basiqueevangelist.flashfreeze.FakeArmorStandEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorStandItem;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ArmorStandItem.class)
+public class ArmorStandItemMixin {
+    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityType;create(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/nbt/NbtCompound;Lnet/minecraft/text/Text;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/SpawnReason;ZZ)Lnet/minecraft/entity/Entity;"))
+    private Entity makeEntity(EntityType<?> entityType, ServerWorld world, NbtCompound itemNbt, Text name, PlayerEntity player, BlockPos pos, SpawnReason spawnReason, boolean alignPosition, boolean invertY, ItemUsageContext context) {
+        if (context.getStack().hasTag() && context.getStack().getTag().contains("OriginalEntityData", NbtElement.COMPOUND_TYPE)) {
+            Entity e = new FakeArmorStandEntity(world, context.getStack().getTag().getCompound("OriginalEntityData").copy());
+            e.setPos(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+            return e;
+        }
+
+        return entityType.create(world, itemNbt, name, player, pos, spawnReason, alignPosition, invertY);
+    }
+
+}
