78acae9fb80135727275d4022443ebb9e9184813
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-07-25 04:23:33 +0300

    preserve item cca components

--- common/src/main/java/me/basiqueevangelist/flashfreeze/mixin/ItemStackMixin.java
+++ common/src/main/java/me/basiqueevangelist/flashfreeze/mixin/ItemStackMixin.java
@@ -1,15 +1,16 @@
 package me.basiqueevangelist.flashfreeze.mixin;
 
 import me.basiqueevangelist.flashfreeze.FlashFreeze;
+import me.basiqueevangelist.flashfreeze.components.ComponentHolder;
 import me.shedaniel.architectury.utils.NbtType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,6 +22,8 @@ public abstract class ItemStackMixin {
     @Shadow @Nullable public abstract NbtCompound getSubTag(String key);
 
     @Shadow private int count;
+
+    @Unique private final ComponentHolder componentHolder = new ComponentHolder();
 
     @Inject(method = "fromNbt", at = @At("HEAD"), cancellable = true)
     private static void makeUnknownIfNeeded(NbtCompound tag, CallbackInfoReturnable<ItemStack> cir) {
@@ -41,5 +44,15 @@ public abstract class ItemStackMixin {
             tag.putByte("Count", (byte) count);
             cir.setReturnValue(tag);
         }
+    }
+
+    @Inject(method = "fromNbt", at = @At("RETURN"))
+    private static void readCCAComponents(NbtCompound nbt, CallbackInfoReturnable<ItemStack> cir) {
+        ((ItemStackMixin)(Object) cir.getReturnValue()).componentHolder.fromTag(nbt);
+    }
+
+    @Inject(method = "writeNbt", at = @At("RETURN"))
+    private void writeCCAComponents(NbtCompound nbt, CallbackInfoReturnable<NbtCompound> cir) {
+        componentHolder.toTag(nbt);
     }
 }
